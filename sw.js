252ce5cdf0725c21fdafff040766367ca20b4ebd
Zhi Yin <zhiyin@users.noreply.github.com>
2026-07-08 16:56:19 +0800

    Fix mobile backgrounds and duel navigation

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
-const cacheName = "ruogu-literacy-v23";
+const cacheName = "ruogu-literacy-v24";
 const assets = [
   "./",
   "./index.html",
+  "./home.html",
   "./game.html",
   "./weapon-library.html",
   "./weapon-preview.html",
@@ -25,56 +26,9 @@ const assets = [
   "./icons/icon-180.png",
   "./icons/icon-192.svg",
   "./icons/icon-512.svg",
-  "./assets/weapons-held/01_jingubang_r.png",
-  "./assets/weapons-held/01_jingubang_l.png",
-  "./assets/weapons-held/02_jiuchidingpa_r.png",
-  "./assets/weapons-held/02_jiuchidingpa_l.png",
-  "./assets/weapons-held/03_jiangyaobaozhang_r.png",
-  "./assets/weapons-held/03_jiangyaobaozhang_l.png",
-  "./assets/weapons-held/04_sanjianliangrendao_r.png",
-  "./assets/weapons-held/04_sanjianliangrendao_l.png",
-  "./assets/weapons-held/05_bajiaoshan_r.png",
-  "./assets/weapons-held/05_bajiaoshan_l.png",
-  "./assets/weapons-held/06_qinglongyanyuedao_r.png",
-  "./assets/weapons-held/06_qinglongyanyuedao_l.png",
-  "./assets/weapons-held/07_zhangbashemao_r.png",
-  "./assets/weapons-held/07_zhangbashemao_l.png",
-  "./assets/weapons-held/08_fangtianhuaji_r.png",
-  "./assets/weapons-held/08_fangtianhuaji_l.png",
-  "./assets/weapons-held/09_qinggangjian_r.png",
-  "./assets/weapons-held/09_qinggangjian_l.png",
-  "./assets/weapons-held/10_qixingbaodao_r.png",
-  "./assets/weapons-held/10_qixingbaodao_l.png",
-  "./assets/weapons-held/11_dashenbian_r.png",
-  "./assets/weapons-held/11_dashenbian_l.png",
-  "./assets/weapons-held/12_fantianyin_r.png",
-  "./assets/weapons-held/12_fantianyin_l.png",
-  "./assets/weapons-held/13_qiankunquan_r.png",
-  "./assets/weapons-held/13_qiankunquan_l.png",
-  "./assets/weapons-held/14_huojianqiang_r.png",
-  "./assets/weapons-held/14_huojianqiang_l.png",
-  "./assets/weapons-held/15_jiulongshenhuozhao_r.png",
-  "./assets/weapons-held/15_jiulongshenhuozhao_l.png",
-  "./assets/weapons-held/16_shuimochanzhang_r.png",
-  "./assets/weapons-held/16_shuimochanzhang_l.png",
-  "./assets/weapons-held/17_shuangbanfu_r.png",
-  "./assets/weapons-held/17_shuangbanfu_l.png",
-  "./assets/weapons-held/18_langyabang_r.png",
-  "./assets/weapons-held/18_langyabang_l.png",
-  "./assets/weapons-held/19_xuehuabingtiejiedao_r.png",
-  "./assets/weapons-held/19_xuehuabingtiejiedao_l.png",
-  "./assets/weapons-held/20_goulianqiang_r.png",
-  "./assets/weapons-held/20_goulianqiang_l.png",
-  "./assets/weapons-held/21_xuantiezhongjian_r.png",
-  "./assets/weapons-held/21_xuantiezhongjian_l.png",
-  "./assets/weapons-held/22_tulongdao_r.png",
-  "./assets/weapons-held/22_tulongdao_l.png",
-  "./assets/weapons-held/23_yitianjian_r.png",
-  "./assets/weapons-held/23_yitianjian_l.png",
-  "./assets/weapons-held/24_jinshejian_r.png",
-  "./assets/weapons-held/24_jinshejian_l.png",
-  "./assets/weapons-held/25_dagoubang_r.png",
-  "./assets/weapons-held/25_dagoubang_l.png"
+  "./assets/backgrounds/07_reading_galaxy_map_mobile.jpg",
+  "./assets/backgrounds/06_cloud_characters_mobile.jpg",
+  "./assets/backgrounds/05_scholar_mountain_mobile.jpg"
 ];
 
 self.addEventListener("install", (event) => {
@@ -107,6 +61,15 @@ self.addEventListener("fetch", (event) => {
       }).catch(() => caches.match(req))
     );
   } else {
-    event.respondWith(caches.match(req).then((cached) => cached || fetch(req)));
+    event.respondWith(
+      caches.match(req).then((cached) => {
+        if (cached) return cached;
+        return fetch(req).then((res) => {
+          const clone = res.clone();
+          caches.open(cacheName).then((cache) => cache.put(req, clone)).catch(() => {});
+          return res;
+        });
+      })
+    );
   }
 });
